8da4bb2aeee7c583f808b3406bc1e3abe1ae26bc
Keith McNulty <keith_mcnulty@mckinsey.com>
2019-05-14 10:02:02 +0100

    Add Eurovision

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # scraping
 Functions for web scraping
+
+1.  Scrape US Billboard charts
+2.  Scrape Eurovision Results from Wikipedia
---
+++ eurovision_scraping.R
@@ -0,0 +1,58 @@
+# required libraries
+library(rvest)
+library(xml2)
+library(dplyr)
+
+#' Get Eurovision final results from history
+#'
+#' @param year date in the form YYYY-MM-DD
+#' @return a dataframe of Eurovision results
+
+
+get_eurovision <- function(year) {
+  
+  # get url from input and read html
+  input <- paste0("https://en.wikipedia.org/wiki/Eurovision_Song_Contest_", year) 
+  chart_page <- xml2::read_html(input, fill = TRUE)
+  
+  
+  # scrape data from any sortable table
+  chart <- chart_page %>% 
+    rvest::html_nodes("#mw-content-text") %>% 
+    xml2::xml_find_all("//table[contains(@class, 'sortable')]")
+  
+  charts <- list()
+  chartvec <- vector()
+  
+  for (i in 1:length(chart)) {
+    assign(paste0("chart", i),
+           # allow for unexpected errors but warn user
+           tryCatch({rvest::html_table(chart[[i]], fill = TRUE)}, error = function (e) {print("Potential issue discovered in this year!")})
+    )
+    
+    
+    charts[[i]] <- get(paste0("chart", i))
+    # only include tables that have Points
+    chartvec[i] <- sum(grepl("Points", colnames(get(paste0("chart", i))))) == 1 & sum(grepl("Category|Venue|Broadcaster", colnames(get(paste0("chart", i))))) == 0 
+  }
+  
+  results_charts <- charts[chartvec]
+  
+  # account for move to semifinals and qualifying rounds
+  if (year < 1956) {
+    stop("Contest was not held before 1956!")
+  } else if (year == 1956) {
+    stop("Contest was held in 1956 but no points were awarded!")
+  } else if (year %in% c(1957:1995)) {
+    results_charts[[1]]
+  } else if (year == 1996) {
+    results_charts[[2]]
+  } else if (year %in% 1997:2003) {
+    results_charts[[1]]
+  } else if (year %in% 2004:2007) {
+    results_charts[[2]]
+  } else {
+    results_charts[[3]]
+  }
+  
+}
